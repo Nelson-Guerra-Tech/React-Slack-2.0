e8168ec69fe2d5366874e82b099f59e7d895f6bf
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-06-24 16:46:16 -0400

    Initial App Setup

---
+++ slack-2-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Slack Build</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ slack-2-react/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAr7UuGd42aH6TlSEE_DaBiS6S00PBO51w',
+  authDomain: 'slack-2-react-clone.firebaseapp.com',
+  projectId: 'slack-2-react-clone',
+  storageBucket: 'slack-2-react-clone.appspot.com',
+  messagingSenderId: '409323433999',
+  appId: '1:409323433999:web:dac83fec695228dabb737a',
+};
